d90a181f9045bd91736140e908d1ae12eb574fe0
sakusaku <oreosagi874@gmail.com>
2024-11-11 09:04:15 +0900

    csv import 更新

--- backend/server/controls/csvInport.go
+++ backend/server/controls/csvInport.go
@@ -4,13 +4,20 @@ package controls
 
 import (
 	"backend-app/server/models"
-	"encoding/csv"
-	"net/http"
-	"strconv"
-	"strings"
-
 	"github.com/labstack/echo/v4"
+	"io"
+	"net/http"
+	"fmt"
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
+
 
 /*
 	REQUIRE_COL_NAMESはCSVファイルの中で必須の列名を含んだ配列。
@@ -22,12 +29,52 @@ var (
 		"支店コード", "管制日付", "得意先正式名称", "配置先支店コード",
 		"配置先正式名称", "隊員番号", "勤務番号", "勤務形態正式名称",
 		"基本開始時間", "基本終了時間", "報告開始時間", "報告終了時間", "得意先番号", "隊員名",
-		"配置先番号"}
+	"配置先番号"}
 )
+
+
+
+type Value struct {
+	Preval any
+	as_int int
+	as_string string
+	is_error error //キャストに失敗した場合のエラーを保持	
+}
+//factorymethod　=>　Value
+func ValueInit(val any) *Value {
+	created_val :=  &Value{
+		Preval : val,
+	}
+	//anyを文字列にキャスト　->「 as_stringに結果を格納
+	created_val.To_string()
+	//as_stringを数値に変換
+	created_val.To_int()
+	
+	return created_val
+}
+
+func (v *Value) To_int() {
+	number,err := strconv.Atoi(v.as_string)
+	if err != nil {
+		v.is_error = err
+		return 
+	}
+	v.as_int = number
+}
+
+func (v *Value) To_string() {
+	if  casted_string,err := v.Preval.(string);err != nil {
+		v.is_error = err
+		return 	
+	}
+	v.as_string = casted_string
+}	
+
+
 
 type CsvTable struct { //CSVをプログラムで扱いやすい形にしたもの。基本的には
 	header map[string]int      //列名と列数の辞書
-	rows   []map[string]string //列名と値の辞書を配列で格納した
+	rows   []map[string]*Value //列名と値の辞書を配列で格納した
 }
 
 // コンストラクタ
@@ -39,13 +86,13 @@ func CreateCsVTable(reader csv.Reader) (*CsvTable, error) {
 	}
 	//ヘッダー（列名から列数のマップ）を生成
 	headerMap := make(map[string]int)
-	rows := []map[string]string{}
+	rows := []map[string]*Value{}
 	//ヘッダー辞書を作製
 	for index, colName := range headerRow {
 		headerMap[colName] = index
 	}
 
-	//全体の行のデータをgoの構造体に変換する。
+	//全体の行のデータをgoの構造体に変換する。	
 	for {
 		//行ごとを読み取り。エラーであればループを抜ける
 		row, err := reader.Read()
@@ -53,16 +100,20 @@ func CreateCsVTable(reader csv.Reader) (*CsvTable, error) {
 			break
 		}
 		//一時的な行を表す辞書
-		temp_row_map := make(map[string]string)
-
+		temp_row_map := make(map[string]Value)
+		
 		for key, val := range headerMap {
 			index := val
-			temp_row_map[key] = row[index]
+			temp_value := ValueInit( row[index] )
+			if temp_value.has_error != nil {
+				return nil,temp_value.has_error
+			}	
+			temp_row_map[key] = temp_value
 		}
 		//辞書をスライスに格納
 		rows = append(rows, temp_row_map)
 	}
-
+	
 	return &CsvTable{
 		header: headerMap,
 		rows:   rows,
@@ -70,25 +121,30 @@ func CreateCsVTable(reader csv.Reader) (*CsvTable, error) {
 
 }
 
+
+
 // このCSVテーブルをDBへ登録しても問題ないか確認するメソッド
 func (ct *CsvTable) checkReqireColmuns() ([]string, bool) {
 	if len(ct.header) == 0 || len(ct.rows) == 0 {
 		return nil, false
 	}
 
+
 	//列名の確認　（要件を満たす列名を保持しているかの確認）
 	missingColumns := []string{}
+
 
 	for _, req_col_name := range REQUIRE_COL_NAMES {
 		if _, exists := ct.header[req_col_name]; !exists {
 			missingColumns = append(missingColumns, req_col_name)
 		}
 	}
+	
 
 	if len(missingColumns) > 0 {
 		return missingColumns, false
 	}
-
+	
 	//成功
 	return nil, true
 }
@@ -96,36 +152,24 @@ func (ct *CsvTable) checkReqireColmuns() ([]string, bool) {
 // このメソッドを実行すると,個別に勤怠データーとして登録できる構造体に変換する。
 func (ct *Csvtable) To_AttendanceRecords() ([]*models.AttendanceRecord, error) {
 
-	createToAttendacneRecord := func(row map[string]string) *models.AttendanceRecord {
-		//rowからTimeRecordを作製する
-
-		to_int := func(number string) int {
-			num, err := strconv.Atoi(number)
-			if err != nil {
-				return 0
-			}
-			return num
-		}
-
-		//まだ初期値の入れる値が不確定なので、一旦保留
+	createToAttendacneRecord := func (row map[string]Value) *models.AttendacneRecord {
 		return &models.AttendanceRecord{
-			ManageID:   manage_id, //これが基本となる値。
-			EmpID:      row["社員番号"],
-			LocationID: row["配置先番号"], //
-
-			TimeRecords: models.createToTimeRecords(row, manage_id),
+			ManageID:   row["管制番号"].as_int, //これが基本となる値。
+			EmpID:      row["社員番号"].as_int,
+			LocationID: row["配置先番号"].as_int, //
+			
+			TimeRecords: models.createToTimeRecords(row),
 		}
 
-	}
-
+	}	
+	
 	var result_array []*models.AttendanceRecord
 	for _, row := range ct.rows {
 		result_array = append(result_array, createToAttendacneRecord(row))
 	}
 
-	return result_array, nil
-
-}
+	return result_array,nil
+}	
 
 // CSVファイルのインポート
 func CsvImportHandler(c echo.Context) error {
@@ -135,13 +179,28 @@ func CsvImportHandler(c echo.Context) error {
 		return c.String(http.StatusBadRequest, "csv file not found")
 	}
 
+	reader := csv.NewReader(strings.NewReader(import_csv))
+
+	csv_table,value_error := CreateCsVTable(reader)
+	if value_error != nil {
+		return c.String(http.StatusBadRequest,"CSVの値に問題があります。確認してくださ。")
+	}
+
+
 	//CSVを確認し、
-	missingColumns, isok := CheckReqireColumns(import_csv)
-	if !isok { //JSONで足りない列を配列で返す。
+	missingColumns, ok := csv_table.checkReqireColmuns()
+	if !ok { //JSONで足りない列を配列で返す。
 		return c.JSON(http.StatusBadRequest, missingColumns)
 	}
 
-	return nil
+	//以下　列名と値に問題ないと判断されたブロック
+
+
+
+
+
+
+
 }
 
 func validateCSV(data string) (bool, string) {
@@ -154,18 +213,18 @@ func validateCSV(data string) (bool, string) {
 	}
 
 	//必要なヘッダーを定義する。
-	reqireHeaders := []string{"Column1", "Column2", "Column3"} //必要なヘッダー名を指定
-	for _, reqireHeader := range reqireHeaders {
-		if !contains(headers, reqireHeader) {
-			return false, "missing reqired header:" + reqireHeader
+	reqireHeaders := []string{"Column1","Column2","Column3"}//必要なヘッダー名を指定
+	for _ , reqireHeader := range reqireHeaders{
+		if !contains(headers,reqireHeader){
+			return false,"missing reqired header:" + reqireHeader
 		}
 	}
 	//必要なヘッダーを読み込む
 }
 
 func contains(slice []string, item string) bool {
-
-	for _, v := range slice {
+	
+	for _,v := range slice {
 		if v == item {
 			return true
 		}
@@ -184,7 +243,7 @@ func saveCSVToDB(data string) error {
 		return err
 	}
 
-	for i := 0; i < len(header); i++ {
+	for i := 0;i < len(header);i++ {
 		HeaderDict[header[i]] = i
 	}
 
