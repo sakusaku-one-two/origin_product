ede314b297095b9f3c8c105d2d2d5b12c92f9bf3
sakusaku <oreosagi874@gmail.com>
2024-11-16 06:20:45 +0900

    udpate

--- backend/server/models/demon.go
+++ backend/server/models/demon.go
@@ -13,8 +13,9 @@ func TimeActionCatcher() {
 	defer close(ACTION_DTO_FROM_CLIENT)
 	
 	var ACTION_DTO_FROM_CLIENT chan server.ActionDTO[TimeRecord] = server.NewChannel_TypeIs[server.ActionDTO[TimeRecord]]("CLIENT_ACTION_TO_DB", 100)
-
+	var TO_BROADCAST chan server.ActionDTO[TimeRecord] = server.FetchChannele_TypeIs[server.ActionDTO[TimeRecord]]("")
 	for time_record := range ACTION_DTO_FROM_CLIENT {
+		new_query := NewQuerySession()
 
 	}
 
