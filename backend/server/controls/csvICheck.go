615252462a54c740673ce65c0abf0719cd82a786
sakusaku <oreosagi874@gmail.com>
2024-11-19 07:14:41 +0900

    update

--- backend/server/controls/csvICheck.go
+++ backend/server/controls/csvICheck.go
@@ -240,57 +240,5 @@ func CsvImportHandler(c echo.Context) error {
 	if err != nil {
 		return c.String(http.StatusBadRequest, "CSVの値に問題があります。確認してくださ。")
 	}
-	c.JSON(http.StatusOK, *reuslt)
-	return nil
-}
-
-// --------------------------------------[以下一旦保護]-------------------------
-
-func validateCSV(data string) (bool, string) {
-	//csvReaderを作製
-	reader := csv.NewReader(strings.NewReader(data))
-	//ヘッダーを読み込む
-	headers, err := reader.Read()
-	if err != nil {
-		return false, "failed to read CSV headers"
-	}
-
-	//必要なヘッダーを定義する。
-	reqireHeaders := []string{"Column1", "Column2", "Column3"} //必要なヘッダー名を指定
-	for _, reqireHeader := range reqireHeaders {
-		if !contains(headers, reqireHeader) {
-			return false, "missing reqired header:" + reqireHeader
-		}
-	}
-	//必要なヘッダーを読み込む
-}
-
-func contains(slice []string, item string) bool {
-
-	for _, v := range slice {
-		if v == item {
-			return true
-		}
-	}
-	return false
-}
-
-func saveCSVToDB(data string) error {
-	//csvリーダーを作製
-	reader := csv.NewReader(strings.NewReader(data))
-	//ヘッダー行の列数と列名の対象表を作製
-	HeaderDict := make(map[string]int)
-	//ヘッダーを読み飛ばす
-
-	if header, err := reader.Read(); err != nil {
-		return err
-	}
-
-	for i := 0; i < len(header); i++ {
-		HeaderDict[header[i]] = i
-	}
-
-	var Attendance_records []models.AttendanceRecord
-	var Time_records []models.TimeRecord
-
+	return c.JSON(http.StatusOK, *reuslt)
 }
