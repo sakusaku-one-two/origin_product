82d99fcba256c4bd8b4c7a00ec7a137ea30c4376
sakusaku <oreosagi874@gmail.com>
2024-11-23 01:36:13 +0900

    update redux midllware websocket

--- forntend/front-app/src/redux/wsRecordSwitch.ts
+++ forntend/front-app/src/redux/wsRecordSwitch.ts
@@ -1,64 +1,80 @@
-import { ActivityIcon } from "lucide-react";
-import { AttendanceRecord,EmployeeRecord,LocationRecord,TimeRecord } from "./taskSlice";
+import { Middleware} from '@reduxjs/toolkit';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import type { AttendanceRecord,EmployeeRecord,LocationRecord,TimeRecord } from "./taskSlice";
 import { Store } from "@reduxjs/toolkit";
-export type WsRecordSwitch = {
-    AttendanceRecord:AttendanceRecord,
-    EmployeeRecord:EmployeeRecord,
-    LocationRecord:LocationRecord,
-    TimeRecord:TimeRecord
-}
+import { WEBSOCKET_CONNECT, WEBSOCKET_SEND_MESSAGE } from './webSocketMiddleweare';
 
-//ウェブソケットの受信メッセージの処理
-export const OnMessageSwitch = (store:Store):((target:MessageEvent<{Action:string,Payload:unknown}>)=>void) => {
-    return (target:MessageEvent<{Action:string,Payload:unknown}>)=>{
-        switch (target.data.Action) {
-            case "ATTENDANCE_RECORD_UPDATE":
-                store.dispatch({type:'ATTENDANCE_RECORD_UPDATE_MESSAGE',payload:<AttendanceRecord>target.data.Payload});
-            break;
-        case "ATTENDANCE_RECORD_DELETE":
-            store.dispatch({type:'ATTENDANCE_RECORD_DELETE_MESSAGE',payload:<AttendanceRecord>target.data.Payload});
-            break;
-        case "TIME_RECORD_UPDATE":
-            store.dispatch({type:'TIME_RECORD_UPDATE_MESSAGE',payload:<TimeRecord>target.data.Payload});
-            break;
-        case "TIME_RECORD_DELETE":
-            store.dispatch({type:'TIME_RECORD_DELETE_MESSAGE',payload:<TimeRecord>target.data.Payload});
-            break;
-        case "EMPLOYEE_RECORD_UPDATE":
-            store.dispatch({type:'EMPLOYEE_RECORD_UPDATE_MESSAGE',payload:<EmployeeRecord>target.data.Payload});
-            break;
-        case "EMPLOYEE_RECORD_DELETE":
-            store.dispatch({type:'EMPLOYEE_RECORD_DELETE_MESSAGE',payload:<EmployeeRecord>target.data.Payload});
-            break;
-        case "LOCATION_RECORD_UPDATE":
-            store.dispatch({type:'LOCATION_RECORD_UPDATE_MESSAGE',payload:<LocationRecord>target.data.Payload});
-            break;
-        case "LOCATION_RECORD_DELETE":
-            store.dispatch({type:'LOCATION_RECORD_DELETE_MESSAGE',payload:<LocationRecord>target.data.Payload});
-            break;
-        default:
-            break;
-        }   
-    };  
-};
+export type ServerActionType = {Action:string,Payload:unknown};
+export type ClientActionType = {type:string,payload:unknown};
 
-export type ActionType = {
+export type CaseType = {
     type:string,
-    payload:unknown
+    ModelType:AttendanceRecord |  TimeRecord |  LocationRecord | EmployeeRecord | null,
+    Action:string
 }
 
-export const ActionTypes = ["ATTENDANCE_RECORD_UPDATE","ATTENDANCE_RECORD_DELETE",
-                            "TIME_RECORD_UPDATE","TIME_RECORD_DELETE",
-                            "EMPLOYEE_RECORD_UPDATE","EMPLOYEE_RECORD_DELETE",
-                            "LOCATION_RECORD_UPDATE","LOCATION_RECORD_DELETE",
-                            "WEBSOCKET_CLOSE"];
+export class CasePattern<T> {
+    type:string;
+    Action:string;
+    ModelType!:T;
+    constructor(type:string,Action:string){
+        this.type = type;
+        this.Action = Action;
+    }
+    parse(payload:unknown):T{
+        return payload as T;
+    }
+    
+}
 
-                            //ウェブソケットの送信メッセージの処理
+export const Cases:CasePattern<AttendanceRecord |  TimeRecord |  LocationRecord | EmployeeRecord | null>[] = [
+    new CasePattern<AttendanceRecord>("ATTENDANCE_RECORD_UPDATE","ATTENDANCE_RECORD_UPDATE_MESSAGE"),
+    new CasePattern<AttendanceRecord>("ATTENDANCE_RECORD_DELETE","ATTENDANCE_RECORD_DELETE_MESSAGE"),
+    new CasePattern<TimeRecord>("TIME_RECORD_UPDATE","TIME_RECORD_UPDATE_MESSAGE"),
+    new CasePattern<TimeRecord>("TIME_RECORD_DELETE","TIME_RECORD_DELETE_MESSAGE"),
+    new CasePattern<LocationRecord>("LOCATION_RECORD_UPDATE","LOCATION_RECORD_UPDATE_MESSAGE"),
+    new CasePattern<LocationRecord>("LOCATION_RECORD_DELETE","LOCATION_RECORD_DELETE_MESSAGE"),
+    new CasePattern<EmployeeRecord>("EMPLOYEE_RECORD_UPDATE","EMPLOYEE_RECORD_UPDATE_MESSAGE"),
+    new CasePattern<EmployeeRecord>("EMPLOYEE_RECORD_DELETE","EMPLOYEE_RECORD_DELETE_MESSAGE"),
+    new CasePattern("CLOSE","CLOSE_MESSAGE"),
+];
 
-export const OnSendMessageSwitch = (socket:WebSocket):((action:ActionType)=>void) => {
-    return (action:ActionType)=>{  
-        if (ActionTypes.includes(action.type)) {
-            socket.send(JSON.stringify({Action:action.type,Payload:action.payload}));
+
+
+
+//サーバーからのメッセージを受け取った際の処理
+export function OnMessage(store:Store,socket:WebSocket){
+    socket.onmessage = (target:MessageEvent<ServerActionType>)=>{
+        const actionType = target.data as ServerActionType;
+        const casePattern:CasePattern<AttendanceRecord |  TimeRecord |  LocationRecord | EmployeeRecord | null>|undefined = Cases.find(c=>c.type === actionType.Action);
+        if (casePattern) {
+            store.dispatch({type:casePattern.Action,payload:casePattern.parse(actionType.Payload)});
         }
     }
+};
+
+//サーバーへのメッセージを送信する際の処理
+export function OnSend(socket:WebSocket,action:ClientActionType):boolean {   
+    const casePattern:CasePattern<AttendanceRecord |  TimeRecord |  LocationRecord | EmployeeRecord | null>|undefined = Cases.find(c=>c.type === action.type);
+    if (casePattern) {
+        socket.send(JSON.stringify({Action:casePattern.type,Payload:casePattern.parse(action.payload)}));
+        return true;
+    }
+    return false;   
+};
+
+
+
+export const wsMiddleware:Middleware = (store:Store)=>(next)=>(action:ClientActionType)=>{
+    let socket:WebSocket|null = null;
+    if (action.type === WEBSOCKET_CONNECT) {
+        socket = new WebSocket("ws://localhost:8080/api/sync");
+        OnMessage(store,socket);
+    }
+    if (socket) {
+        OnSend(socket,action);
+    }
+    return next(action);
+
 };
