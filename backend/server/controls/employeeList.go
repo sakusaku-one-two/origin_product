2526c4b10409ac5c4ee5df02896b44f1c616f934
sakusaku <oreosagi874@gmail.com>
2025-01-26 06:40:23 +0900

    update

--- backend/server/controls/employeeList.go
+++ backend/server/controls/employeeList.go
@@ -12,3 +12,23 @@ func EmployeeListHandler(c echo.Context) error {
 	emps := models.EMPLOYEE_RECORD_REPOSITORY.Cache.Dump()
 	return c.JSON(http.StatusOK, emps)
 }
+
+// 社員の更新
+type RequestDataForEmployee struct {
+	EmployeeRecord models.EmployeeRecord
+}
+
+func UpdateEmployeeHandler(c echo.Context) error {
+	var requestData RequestDataForEmployee
+	if err := c.Bind(&requestData); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	// 直接 EmployeeRecord を Insert 関数に渡す
+	_, err := models.EMPLOYEE_RECORD_REPOSITORY.Cache.Insert(requestData.EmployeeRecord.EmpID, &requestData.EmployeeRecord)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, "社員の更新が完了しました。")
+}
