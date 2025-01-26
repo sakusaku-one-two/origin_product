14d334166cf583346ea8564380ea0d1cf4726de6
saku <saku@kali.jp>
2025-01-26 23:49:43 +0900

    update

--- backend/server/controls/controls.go
+++ backend/server/controls/controls.go
@@ -7,7 +7,6 @@ import (
 // server.goから呼び出して設定済みのechoにhttpハンドラーを追加していく
 func SetupHandlers(e *echo.Echo) *echo.Echo {
 
-
 	//{ CSV関連のAPI 
 	e.POST("/Csvcheck", CsvImportHandler)          //CSV確認用のエンドポイント　各種構造体に変換し既存のレコード構造体と重複や相異を返却
 	e.POST("/InsertRecords", InsertRecordsHandler) //CSVのデータとDBとの相違確認が完了して、登録するJSONデータを受け取るエンドポイント
@@ -22,7 +21,6 @@ func SetupHandlers(e *echo.Echo) *echo.Echo {
 	//GET
 	e.GET("/sync", ActionWebSocketHandler) //ウェブソケット用のアップグレード用のエンドポイント
 	e.GET("/health", HealthCheckHandler)   //ヘルスチェック用のエンドポイント
-	
 	
 	return e
 }
